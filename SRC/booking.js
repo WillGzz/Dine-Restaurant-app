a5c9fe3c44613406368dfa9609f32db4448f9611
Will <Willygomezr1@gmail.com>
2024-12-02 02:44:14 -0500

    form styling

--- SRC/booking.js
+++ SRC/booking.js
@@ -21,6 +21,39 @@ function countPeople(event) {
     }
 }
 
+const arrow = document.getElementsByClassName('dropdown-arrow')[0];
+const dropdownView = document.getElementsByClassName('dropdown')[0];
+const dropDownSelection = document.getElementById('input-time-of-day');
+const amSelection = document.getElementsByClassName('am')[0];
+const pmSelection = document.getElementsByClassName('pm')[0];
+const amCheck = document.getElementById('am-check');
+const pmCheck = document.getElementById('pm-check');
+/*Event delegation
+ Instead of adding individual event listeners to multiple child elements,
+  you attach a single event listener to a common parent element. 
+ This parent element then handles events that bubble up from its child elements.
+*/
+function bookingDropdown() {
+    arrow.classList.toggle('dropdown-arrow-rotate');
+    dropdownView.classList.toggle('dropdown-view');
+}
+
+function selectTime(event) {
+    if (event.target === amSelection) {
+        dropDownSelection.placeholder = amSelection.textContent;
+        amCheck.style.visibility= "visible";
+        pmCheck.style.visibility ="hidden";
+    } else if (event.target === pmSelection) {
+        dropDownSelection.placeholder = pmSelection.textContent;
+         pmCheck.style.visibility ="visible";
+         amCheck.style.visibility ="hidden";
+
+    }
+}
+
 
 minuSign.addEventListener('click', countPeople);
 plusSign.addEventListener('click', countPeople);
+arrow.addEventListener('click', bookingDropdown);
+amSelection.addEventListener('click', selectTime);
+pmSelection.addEventListener('click', selectTime);
